d38631db24c0ecb4941699ac0d5297eddad8e548
fernando prass <fernando.sarturiprass@altran.com>
2020-07-20 10:13:30 +0100

    Add update method to Account class

--- src/API/Contracts/Domain/IAccountRepository.cs
+++ src/API/Contracts/Domain/IAccountRepository.cs
@@ -10,8 +10,18 @@ namespace API.Contracts
         /// <summary>Add an account</summary>
         /// <param name="customerId">Customer identifier</param>
         /// <param name="initialCredit">Initial credit</param>
-        /// <returns>A new object Account</returns>
+        /// <returns>A new Account object</returns>
         Account Add(int customerId, double initialCredit);
+
+        /// <summary> Get an Account by identifier (Id) </summary>
+        /// <param name="id">Account identifier</param>
+        /// <returns>An exists account object</returns>
+        Account GetById(Guid id);
+
+        /// <summary>Update an account</summary>
+        /// <param name="account">Account object</param>
+        /// <returns>The new account object</returns>
+        Account Update(Account account);
     }
 }
 
