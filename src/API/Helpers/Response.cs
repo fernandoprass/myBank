922e33af24525ccf9f7d60f99272502f8e859191
Fernando Prass <fprass@gmail.com>
2020-07-22 21:11:56 +0100

    Create a new Exception class (#20)
    * Create a new Exception class
    
    * Update dotnet-core.yml
    
    Add new version of  action script
    
    Co-authored-by: fernando prass <fernando.sarturiprass@altran.com>

--- src/API/Helpers/Response.cs
+++ src/API/Helpers/Response.cs
@@ -4,9 +4,10 @@
     public class Response : BaseResponse
     {
         public static Response Success = new Response(0, "The requested operation was successfully performed");
-        public static Response Failure = new Response(1, "Failed to perform requested operation, try again later");
-        public static Response InvalidCustomer = new Response(2, "Invalid customer identifier");
-        public static Response InvalidInitialValue = new Response(3, "The initial value must be greater than or equal to zero");
+        public static Response OperationError = new Response(1, "operation error (not identified)");
+        public static Response InputOutError = new Response(2, "It was not possible to save the Json file");
+        public static Response InvalidCustomer = new Response(3, "Invalid customer identifier");
+        public static Response InvalidAccount = new Response(4, "Invalid account identifier");
 
         public Response(int id, string name)
           : base(id, name)
